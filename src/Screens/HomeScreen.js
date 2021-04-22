11bfca9144c9b6011af93596aff4d4edf63a9ca3
Christian Martinez <mar13137@byui.edu>
2021-04-21 22:43:48 -0500

    Save inputs

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react'
-import { Text, StyleSheet, View, SafeAreaView, TextInput, Button } from 'react-native'
+import React, { useState, Component } from 'react'
+import { Text, StyleSheet, SafeAreaView, TextInput, Button } from 'react-native'
 import { useNavigation } from '@react-navigation/native';
+import {Picker} from '@react-native-picker/picker';
 
 class HomeScreen extends Component {
+    state = {
+        paycheck: 0,
+        occurences: 0,
+        expenses: 0,
+        towardsSavings: 0,
+        towardsInvestments: 0,
+    };
+
+    onSubmit = () => {
+        console.log('state', this.state);
+        this.props.navigation.navigate('Current Savings');
+    }
+
     render() {
-        const { navigation } = this.props;
         return (
             <SafeAreaView style={styles.container}>
                 <Text> Paycheck Amount </Text>
-                <TextInput placeholder='$800' />
+                <TextInput 
+                placeholder='$800' 
+                onChangeText={paycheck => this.setState({paycheck})}
+                />
                 <Text> Paychecks per month </Text>
-                <TextInput placeholder='2' />
+                <TextInput 
+                placeholder='2' 
+                onChangeText={occurences => this.setState({occurences})}
+                />
                 <Text> Monthly Expenses </Text>
-                <TextInput placeholder='$1000' />
+                <TextInput 
+                placeholder='$1000' 
+                onChangeText={expenses => this.setState({expenses})}
+                />
                 <Text> Monthly towards Savings </Text>
-                <TextInput placeholder='$50' />
+                <TextInput 
+                placeholder='$50' 
+                onChangeText={towardsSavings => this.setState({towardsSavings})}
+                />
                 <Text> Monthly towards Investments </Text>
-                <TextInput placeholder='$50' />
+                <TextInput 
+                placeholder='$50' 
+                onChangeText={towardsInvestments => this.setState({towardsInvestments})}
+                />
                 <Button
-      title="Next"
-      onPress={() => {
-        navigation.navigate('Current Savings');
-      }}
-    />
+                title="Next"
+                onPress={this.onSubmit}
+                />
             </SafeAreaView>
         )
     }
